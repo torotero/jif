03a6631e0681b4035d734240c0fbb3f6763c3902
torotero <ganza.toro@gmail.com>
2015-04-09 17:03:24 +0700

    finishing js navigation link

--- js/custom.js
+++ js/custom.js
@@ -10,53 +10,118 @@ $(document).ready(function(){
     
     
     
-    
+    //link home
     
     $("#navi_home").click(function(){
-        $("#content").fadeOut("slow");
+        $.each([ "#content_event","#content_news","#content_contact","#content_idol" ], function( index, value ) {
+            //alert( index + ": " + value );
+            
+            $(value).fadeOut("slow",function(){
+                $("#content").show("slow");
+            });
+            
+            
+            
+        })
     
     });
     
     
+    
+    
+   
+    //link event
     
     
     $("#navi_event").click(function(){
-        
-        $("#content").fadeOut("slow",function(){
-            $("#content_event").show("slow");
-        });
-        
-        
-        
-        
-        
-        
-        
-        
+        $.each([ "#content","#content_news","#content_contact","#content_idol" ], function( index, value ) {
+            //alert( index + ": " + value );
+            
+            $(value).fadeOut("slow",function(){
+                $("#content_event").show("slow");
+            });
+            
+            
+            
+        })
     
     });
     
+    
+    
+    //link news
     
       $("#navi_news").click(function(){
-        $("#content").fadeOut();
+        $.each([ "#content","#content_event","#content_contact","#content_idol" ], function( index, value ) {
+            //alert( index + ": " + value );
+            
+            $(value).fadeOut("slow",function(){
+                $("#content_news").show("slow");
+            });
+            
+            
+            
+        })
     
     });
     
     
-      $("#navi_contact").click(function(){
-        $("#content").fadeOut();
+    
+    
+    
+    
+    
+    //link contact
+    
+    
+    
+    $("#navi_contact").click(function(){
+        $.each([ "#content","#content_event","#content_news","#content_idol" ], function( index, value ) {
+            //alert( index + ": " + value );
+            
+            $(value).fadeOut("slow",function(){
+                $("#content_contact").show("slow");
+            });
+            
+            
+            
+        })
     
     });
     
     
     
-      $("#navi_idol").click(function(){
-        $("#content").fadeOut();
+    
+    
+    //link idol
+    
+    
+    
+    $("#navi_idol").click(function(){
+        $.each([ "#content","#content_event","#content_news","#content_contact"], function( index, value ) {
+            //alert( index + ": " + value );
+            
+            $(value).fadeOut("slow",function(){
+                $("#content_idol").show("slow");
+            });
+            
+            
+            
+        })
     
     });
     
     
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
 });
 
 
